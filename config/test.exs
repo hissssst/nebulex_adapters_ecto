101edf042a7befbd4e18a21df5fe27a6431ff1c2
hissssst <csqcsqcsq@yandex.ru>
2024-01-27 17:15:43 +0400

    Update docs and move config

--- config/test.exs
+++ config/test.exs
@@ -8,3 +8,12 @@ config :nebulex_adapters_ecto, Nebulex.Adapters.EctoTest.Cache,
   gc_timeout: 1000
 
 config :logger, level: :info
+
+config :nebulex_adapters_ecto, Nebulex.Adapters.EctoTest.Repo,
+  database: "nebulex_adapters_ecto_repo",
+  password: System.fetch_env!("POSTGRES_PASSWORD"),
+  hostname: System.fetch_env!("POSTGRES_HOST"),
+  username: System.fetch_env!("POSTGRES_USER"),
+  port: System.fetch_env!("POSTGRES_PORT")
+
+config :nebulex_adapters_ecto, ecto_repos: [Nebulex.Adapters.EctoTest.Repo]
